0a846ba8014d4c4332d1f0ccab377e59edc910a7
Thomas Gielen <thomas.gielen@student.hogent.be>
2025-05-29 14:22:20 +0200

    added character to domain

---
+++ src/Chatacter Generator/src/main/java/com/dnd/charactergenerator/domain/Skills.java
@@ -0,0 +1,34 @@
+package com.dnd.charactergenerator.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Skills {
+    private int acrobatics;
+    private int animalHandling;
+    private int arcana;
+    private int athletics;
+    private int deception;
+    private int history;
+    private int insight;
+    private int intimidation;
+    private int investigation;
+    private int medicine;
+    private int nature;
+    private int perception;
+    private int performance;
+    private int persuasion;
+    private int religion;
+    private int sleightOfHand;
+    private int stealth;
+    private int survival;
+
+}
