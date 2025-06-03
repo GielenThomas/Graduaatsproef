61d7bd3d6b9356a6e7bc18627e4afc4d6a8ee283
Thomas Gielen <thomas.gielen@student.hogent.be>
2025-06-03 15:52:06 +0200

    added characterClass to domain

--- src/Chatacter Generator/src/main/java/com/dnd/charactergenerator/domain/CharacterClass.java
+++ src/Chatacter Generator/src/main/java/com/dnd/charactergenerator/domain/CharacterClass.java
@@ -16,33 +16,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Character {
+public class CharacterClass {
     @Id
     @JdbcTypeCode(SqlTypes.CHAR)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
-    @ManyToOne
-    private Race race;
-    @OneToOne
-    private CharacterClass characterClass;
-    private int level;
-    private String background;
-    private String alignment;
-    private int xp;
-    private Abilities abilities;
-    private Skills skills;
-
-    private int armorClass;
-    private int speed;
-
-    private int maxHealth;
-    private int currentHealth;
-
-    @OneToMany
-    private List<Spell> spells;
-
-    @OneToMany
-    private List<Feat> feats;
-
+    private int hitDie;
+    @ElementCollection(targetClass = String.class)
+    private List<String> proficiencies;
 }
